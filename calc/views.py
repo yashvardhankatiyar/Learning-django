b12917d1f0f2ddd94b6664caad81f3318e5cb7e0
yashvardhan katiyar <ykatiyar0902@gmail.com>
2026-02-10 14:26:04 +0530

    this is learning django - 2, whihc is doing is adding 2 numebrs using get methods but its not safe so next we shift to another

--- calc/views.py
+++ calc/views.py
@@ -4,4 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request) :
-    return HttpResponse("hello world !")
+    return render(request,'home.html')
+    
+def add(request) :
+    #number 1
+    num1 = int(request.GET['num1'])
+    #number 2
+    num2 = int(request.GET['num2'])
+    result  = num1 + num2
+    return render(request, 'result.html', {'result' : result})
--- learningDjango/settings.py
+++ learningDjango/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'learningDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,3 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+print("base dir : ", BASE_DIR)
